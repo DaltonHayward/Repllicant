ba2fc6065c88333d67b1c38f7a58b6ad05f1a3dc
Michael Hernandez <mah768@usask.ca>
2024-01-31 10:26:59 -0600

    added idle and walk animation

--- Assets/PreFabs/testscript.cs
+++ Assets/PreFabs/testscript.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class testscript : MonoBehaviour
 {
+
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += Vector3.forward * Time.deltaTime * 10.0f;
+            animator.SetBool("isMoving", true);
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.position += Vector3.left * Time.deltaTime * 10.0f;
+            animator.SetBool("isMoving", true);
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.position += Vector3.back * Time.deltaTime * 10.0f;
+            animator.SetBool("isMoving", true);
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += Vector3.right * Time.deltaTime * 10.0f;
+            animator.SetBool("isMoving", true);
         }
     }
 }
