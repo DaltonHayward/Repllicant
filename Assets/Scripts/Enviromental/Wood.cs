8a05f8e79366f4be1f12d3912dd0ff9cc3cc1ae2
AllahGold00 <98930392+AllahGold00@users.noreply.github.com>
2024-02-11 23:32:06 -0600

    Modified Collectible script and created a stone script

---
+++ Assets/Scripts/Enviromental/Wood.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wood : Collectible
+{
+    public GameObject dropWhenStoned; // drops when tree is stoned
+    [HideInInspector]public GameObject dropItemStart; // drops when tree isn't stoned
+
+    public bool isStoned = false;
+
+    public Material stoneMaterial;
+    public Material woodMaterial;
+
+    private void Start()
+    {
+        dropItemStart = sureToDrop;
+    }
+
+    // change tree to stoned; changes drop and material
+    public void Stoned()
+    {
+        isStoned = true;
+        sureToDrop = dropWhenStoned;
+        GetComponent<Renderer>().material = stoneMaterial;
+    }
+
+    public void UnStoned() // changes tree back to normal
+    {
+        isStoned = false;
+        sureToDrop = dropItemStart;
+        GetComponent<Renderer>().material = woodMaterial;
+    }
+
+}
