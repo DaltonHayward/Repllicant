f9636c90a7e7d4b5fd62da5287c319e47d052f95
gloria-hello <105770648+gloria-hello@users.noreply.github.com>
2024-02-14 14:18:11 -0600

    Fixed Weather Manager Compile

--- Assets/Scripts/World/WeatherManager.cs
+++ Assets/Scripts/World/WeatherManager.cs
@@ -8,7 +8,6 @@ public class WeatherManager : MonoBehaviour
     [SerializeField, Range(0f, 1f)] float SnowIntensity;
     [SerializeField, Range(0f, 1f)] float FogIntensity;
     [SerializeField, Range(0f, 1f)] float HailIntensity;
-
     // Start is called before the first frame update
     void Start()
     {
