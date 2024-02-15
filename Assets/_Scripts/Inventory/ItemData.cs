3075e9a80721ad2446056fe97f603df65881bb5a
Dalton Hayward <dalton_hayward@hotmail.ca>
2024-02-14 23:29:31 -0600

    Inventory

---
+++ Assets/_Scripts/Inventory/ItemData.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemData : ScriptableObject
+{
+    // Start is called before the first frame update
+    public int width;
+    public int height;
+    public Sprite itemIcon;
+    public GameObject thisItemFab;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void construct(int width, int height, Sprite itemIcon, GameObject prefab)
+    {
+        this.width = width;
+        this.height = height;
+        this.itemIcon = itemIcon;
+        this.thisItemFab = prefab;
+    }
+}
