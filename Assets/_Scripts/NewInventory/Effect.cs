9d404783e5476e322ad8c07f1a97e0aa5521afe7
Jesster2829 <jessterman.haug@gmail.com>
2024-03-04 22:37:07 -0600

    started working on the Logic for Effect

--- Assets/_Scripts/NewInventory/Effect.cs
+++ Assets/_Scripts/NewInventory/Effect.cs
@@ -6,23 +6,20 @@ using UnityEngine;
 
 public abstract class Effect : MonoBehaviour
 {
-    // Start is called before the first frame update
     String channel; 
     float EnvBroadcastRange;
     int InvBroadcastRange;
     bool inInventory;
     float EmitFrequency; 
     IEnumerator emissionCoroutine;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
+    /// <summary>
+    /// sets the parameters of the Effect
+    /// </summary>
+    /// <param name="channel"> What signal you want to send/ items to target</param>
+    /// <param name="EnvBroadcastRange"> A unit measurement to effect in a raidius in the env group  </param>
+    /// <param name="InvBroadcastRange"> the grid radius that the item will send out</param>
+    /// <param name="EmitFrequency"> how often the signal will be sent out</param>
     void setParamerters(String channel , float EnvBroadcastRange , int InvBroadcastRange , float EmitFrequency)
     {
         this.channel = channel;
@@ -30,8 +27,38 @@ public abstract class Effect : MonoBehaviour
         this.InvBroadcastRange = InvBroadcastRange;
         this.EmitFrequency = EmitFrequency;
     }
-    void CoEmit(){
 
+    /// <summary>
+    /// Emits a signmal to all the subscribers in the range of the emitter 
+    ///
+    /// </summary>
+    /// <returns> Nothing </returns>
+     private IEnumerator CoEmit()
+    {
+        if (inInventory)
+        {
+            //no logic yet
+        }
+        else
+        {
+            while (true)
+            {
+                print(gameObject.name);
+                Collider2D[] targets = Physics2D.OverlapCircleAll(transform.position, EnvBroadcastRange);
+
+                foreach (Collider2D c in targets)
+                {
+                    ISubscriber r = c.GetComponent<ISubscriber>();
+                    if (r != null)
+                    {
+                        r.ReceiveMessage(channel);
+                    }
+                }
+
+                yield return new WaitForSeconds(EmitFrequency);
+            }
+        }
+ 
     }
   
 }
