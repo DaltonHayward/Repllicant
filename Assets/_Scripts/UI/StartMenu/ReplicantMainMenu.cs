dc032d465ca10371796ffcab8e0e67b1749ad9f6
gloria-hello <105770648+gloria-hello@users.noreply.github.com>
2024-03-06 18:31:41 -0600

    Main Menu Implementation

---
+++ Assets/_Scripts/UI/StartMenu/ReplicantMainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using ReplicantPackage;
+
+public class ReplicantMainMenu : MenuWithProfiles
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public override void StartGame()
+    {
+        
+    }
+}
