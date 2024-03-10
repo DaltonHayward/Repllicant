3a1d118835dcc467dca91eecfb291d574f5667b6
gloria-hello <105770648+gloria-hello@users.noreply.github.com>
2024-03-10 14:53:07 -0600

    Plug-In Updates

--- Assets/_Scripts/UI/StartMenu/ReplicantMainMenu.cs
+++ Assets/_Scripts/UI/StartMenu/ReplicantMainMenu.cs
@@ -6,24 +6,5 @@ using ReplicantPackage;
 
 public class ReplicantMainMenu : MenuWithProfiles
 {
-    // Start is called before the first frame update
-    void Awake()
-    {
-        
-    }
-
-    // Update is called once per frame
-<<<<<<< Updated upstream
-    /*public override void StartGame()
-    {
-        SceneManager.LoadScene("T - Parker");
-    }*/
-=======
-    /*
-    public override void StartGame()
-    {
-        SceneManager.LoadScene("Main");
-    }
-    */
->>>>>>> Stashed changes
+   
 }
