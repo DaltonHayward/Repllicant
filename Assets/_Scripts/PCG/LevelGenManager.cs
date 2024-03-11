e333184382abbb5d7f7b97e8cf22bfcdf079152e
AllahGold00 <98930392+AllahGold00@users.noreply.github.com>
2024-03-11 11:32:31 -0600

    - moved generation and navmesh rebuild onto a manager to fix lag on start

--- Assets/_Scripts/PCG/LevelGenManager.cs
+++ Assets/_Scripts/PCG/LevelGenManager.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class LevelGenManager : MonoBehaviour
 {
     // generators
-    [SerializeField] private Transform levelgenerators;
-    [SerializeField] private Transform enemygenerators;
+    [SerializeField] private Transform levelGenerators;
+    [SerializeField] private Transform enemyGenerators;
 
     // ai
     [SerializeField] private GameObject nav_Mesh;
@@ -16,13 +16,33 @@ public class LevelGenManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Generate();
+        GenerateEnemies();
+        StartCoroutine(DelayBake());
+    }
+
+    // Activate all level generators
+    private void Generate()
+    {
+        foreach (Transform levelgenerator in levelGenerators) 
+        {
+            levelgenerator.GetComponent<LevelGenerator>().GenerateLevel();
+        }
+    }
+
+    // Activate all enemy generators
+    private void GenerateEnemies()
+    {
+        foreach (Transform levelgenerator in enemyGenerators)
+        {
+            levelgenerator.GetComponent<LevelGenerator>().GenerateLevel();
+        }
     }
 
     // Delay navmesh rebuild till after level gen
     private IEnumerator DelayBake()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(1f);
         nav_Mesh.GetComponent<NavMeshSurface>().BuildNavMesh();
     }
 }
