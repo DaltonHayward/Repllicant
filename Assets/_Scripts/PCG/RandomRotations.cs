dc2e2b7a80197a36817cd44f5dc0dcbc891456c9
AllahGold00 <98930392+AllahGold00@users.noreply.github.com>
2024-03-13 23:45:42 -0600

    - fix for static drops

--- Assets/_Scripts/PCG/RandomRotations.cs
+++ Assets/_Scripts/PCG/RandomRotations.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 // this script randomizes the rotation of ALL children of the object it is made a component of
 public class RandomRotations : MonoBehaviour
 {
+    [SerializeField] GameObject sureToDrop;
+    [SerializeField] GameObject dropWhenStoned;
+
     // Start is called before the first frame update
     void Start()
     {
         foreach (Transform t in transform)
         {
             t.transform.rotation = Quaternion.Euler(t.localEulerAngles.x, Random.Range(0, 360), t.localEulerAngles.z);
+            if (sureToDrop == null && dropWhenStoned == null) 
+            { 
+            t.GetComponent<Wood>().sureToDrop = sureToDrop;
+            t.GetComponent<Wood>().dropWhenStoned = dropWhenStoned;
+            }
         }
     }
 }
