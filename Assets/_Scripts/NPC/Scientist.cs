9fef4c817e2ed4601d424f2b317d350eb9cca697
AllahGold00 <98930392+AllahGold00@users.noreply.github.com>
2024-03-18 11:28:26 -0600

    - added heal to interaction with scientist

--- Assets/_Scripts/NPC/Scientist.cs
+++ Assets/_Scripts/NPC/Scientist.cs
@@ -5,9 +5,27 @@ using UnityEngine;
 public class Scientist : MonoBehaviour
 {
     [SerializeField] ProgressManager progressManager;
+    private GameObject player;
+    // keep track of if player has interacted with this npc in this scene instance
+    public bool interacted = false;
+
+    public void Start()
+    {
+        // Find the player GameObject by its tag
+        player = GameObject.FindWithTag("Player");
+
+        if (player == null)
+        {
+            Debug.LogError("Player GameObject not found! Make sure you have tagged your player GameObject correctly.");
+        }
+    }
 
     public void SendToBase()
     {
-        progressManager.scientist = true;
+        if (!interacted)
+        {
+            progressManager.scientist = true;
+            player.GetComponent<PlayerHealth>().Heal(player.GetComponent<PlayerHealth>().maxHealth);
+        }
     }
 }
