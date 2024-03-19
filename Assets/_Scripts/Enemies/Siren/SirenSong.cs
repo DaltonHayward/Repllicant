754246e2ebc620af48b4a5b68baa35b4c28d8b48
Lucas <ldh029@usask.ca>
2024-03-18 21:25:38 -0600

    cleaning up code a bit

--- Assets/_Scripts/Enemies/Siren/SirenSong.cs
+++ Assets/_Scripts/Enemies/Siren/SirenSong.cs
@@ -8,32 +8,13 @@ public class SirenSong : MonoBehaviour
     private float broadcastRange;
     private string channel;
 
-    public Material areaEffectMaterial; // Assign a material with transparency
-    public float maxScale; // Maximum scale of the effect
-    public Color safeColor = Color.green; // Color when player is outside the charm's effect
-    public Color dangerColor = Color.red; // Color when player is within the charm's effect
-    private GameObject visualCueObject; // The object that will show the visual cue
-    private float currentScale = 1.0f;
-    private bool isIncreasing = true;
 
     private IEnumerator emissionCoroutine;
-
-    private void Awake()
-    {
-        //CreateVisualCue();
-    }
-
-    private void Update()
-    {
-        //PulseEffect();
-        //UpdateColorBasedOnPlayerDistance();
-    }
 
     public void SetParameters(float freq, float range, string ch)
     {
         emitFrequency = freq;
         broadcastRange = range;
-        maxScale = range;
         channel = ch;
         emissionCoroutine = CoEmit();
         StartCoroutine(emissionCoroutine);
@@ -65,34 +46,5 @@ public class SirenSong : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, broadcastRange);
-    }
-
-    void CreateVisualCue()
-    {
-        visualCueObject = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-        Destroy(visualCueObject.GetComponent<Collider>()); // We don't need the collider
-        visualCueObject.transform.SetParent(transform, false);
-        visualCueObject.transform.localScale = new Vector3(maxScale, 0.03f, maxScale); // Flat cylinder
-        visualCueObject.transform.localPosition = Vector3.zero; // Centered on the Siren
-        visualCueObject.GetComponent<Renderer>().material = areaEffectMaterial;
-    }
-
-    void PulseEffect()
-    {
-        visualCueObject.transform.localScale = new Vector3(broadcastRange, 0.01f, broadcastRange);
-    }
-
-    void UpdateColorBasedOnPlayerDistance()
-    {
-        float distance = Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position);
-
-        if (distance <= broadcastRange)
-        {
-            visualCueObject.GetComponent<Renderer>().material.color = Color.Lerp(dangerColor, safeColor, distance / broadcastRange);
-        }
-        else
-        {
-            visualCueObject.GetComponent<Renderer>().material.color = safeColor;
-        }
     }
 }
