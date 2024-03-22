fb7ce7895abde547f2c916ba751dd42ac8260f91
gloria-hello <105770648+gloria-hello@users.noreply.github.com>
2024-03-21 22:33:44 -0600

    AudioManager Fixes

--- Assets/_Scripts/Audio/FMODEvents.cs
+++ Assets/_Scripts/Audio/FMODEvents.cs
@@ -9,10 +9,10 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField] public EventReference ambience { get; private set; }
 
     [field: Header("Music")]
-    [field: SerializeField] public EventReference music { get; private set; }
+    [field: SerializeField] public EventReference betaMusic { get; private set; }
 
     [field: Header("Player SFX")]
-    [field: SerializeField] public EventReference playerFootsteps { get; private set; }
+    [field: SerializeField] public EventReference footStepsGrass { get; private set; }
 
 
     public static FMODEvents instance { get; private set; }
