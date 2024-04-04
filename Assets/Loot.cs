d7455a1c911fde1917cc3983d630925336ddf094
Jesster2829 <jessterman.haug@gmail.com>
2024-04-03 23:25:08 -0600

    Got Loot to work

---
+++ Assets/Loot.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ItemGrid lootGrid;
+    public InventoryController inventoryController;
+    public Canvas LootUI;
+    private bool isChestOpen = false;
+    void Start()
+    {
+        ItemGrid lootgrid = GetComponentInChildren<ItemGrid>(); 
+        InventoryController.instance.RollLoot(lootgrid);
+    }    
+    public void OpenTheStash()
+    {
+        PlayerController player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        if (player._playerState == PlayerController.State.MOVING || player._playerState == PlayerController.State.STANDING || player._playerState == PlayerController.State.INTERACTING)
+        {
+            LootUI.enabled = !isChestOpen;
+            InventoryController.instance.playerCanvas.enabled = !isChestOpen;
+            isChestOpen = !isChestOpen;
+            if (isChestOpen)
+            {
+                player.SetState(PlayerController.State.INTERACTING);
+            }
+            else
+            {
+                player.SetState(PlayerController.State.STANDING);
+            }
+        }
+    }
+
+    
+}
