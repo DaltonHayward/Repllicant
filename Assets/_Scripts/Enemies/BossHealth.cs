b8eff9b66529babe4ee506c1b3e42252fd9228fc
Lucas <ldh029@usask.ca>
2024-04-04 16:34:25 -0600

    cleaning up siren boss

--- Assets/_Scripts/Enemies/BossHealth.cs
+++ Assets/_Scripts/Enemies/BossHealth.cs
@@ -5,84 +5,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//[RequireComponent(typeof(DamageIndicator))]
+[RequireComponent(typeof(DamageIndicator))]
 
 public class BossHealth : MonoBehaviour
 {
+    [SerializeField]
+    public float maxHealth = 250f; // Boss Max health
+    public float currentHealth; // Boss current health
+
+    public GameObject slider;
     public GameObject boss;
-    public Slider healthSlider;
-    public Slider easeSlider;
-    public float maxHealth;
-    public float health;
 
-    private float lerpSpeed = 0.05f;
 
-    float timeUntilHidden = 0f;
-
-    private void Awake()
+    void Start()
     {
-
+        StartCoroutine(RefreshHPBar(0.5f));
     }
-    private void Start()
+
+    public void Update()
     {
-        if (boss.GetComponent<Siren> != null || boss.GetComponent<COW> != null)
+        if (boss.GetComponent<Siren>() != null)
         {
-            maxHealth = boss.transform.hp;
-            health = maxHealth;
-            healthSlider.maxValue = maxHealth;
-            easeSlider.maxValue = maxHealth;
+            currentHealth = Mathf.RoundToInt(boss.GetComponent<Siren>().hp);
+            slider.GetComponent<HealthBarText>().ChangeHealthSlider(currentHealth, maxHealth);
+        }
+        
+        if (boss.GetComponent<COW>() != null)
+        {
+            currentHealth = Mathf.RoundToInt(boss.GetComponent<COW>().hp);
+            slider.GetComponent<HealthBarText>().ChangeHealthSlider(currentHealth, maxHealth);
         }
     }
 
-    private void Update()
+    IEnumerator RefreshHPBar(float seconds)
     {
-        // billboard the health bar to always face camera
-        //transform.LookAt(transform.position + Camera.main.transform.forward);
-
-        // update hidden value on damage
-        if (health != boss.transform.hp)
-        {
-            timeUntilHidden = 3f; // how long until the health bar will be hidden again
-        }
-        // update health bar to match enemies health
-        health = boss.transform.hp;
-
-
-        if (healthSlider.value != health)
-        {
-            healthSlider.value = health;
-        }
-
-
-        if (healthSlider.value != easeSlider.value)
-        {
-            easeSlider.value = Mathf.Lerp(easeSlider.value, health, lerpSpeed);
-        }
-
-        // calculate time until the bar is hidden from player
-        timeUntilHidden -= Time.deltaTime;
-        if (healthSlider != null)
-        {
-            if (timeUntilHidden <= 0)
-            {
-                timeUntilHidden = 0;
-                healthSlider.gameObject.SetActive(false);
-                easeSlider.gameObject.SetActive(false);
-            }
-            else
-            {
-                if (!healthSlider.gameObject.activeInHierarchy)
-                {
-                    healthSlider.gameObject.SetActive(true);
-                    easeSlider.gameObject.SetActive(true);
-                }
-            }
-            if (healthSlider.value <= 0)
-            {
-                Destroy(healthSlider.gameObject);
-                Destroy(easeSlider.gameObject);
-            }
-        }
-
+        yield return new WaitForSeconds(seconds);
+        slider.GetComponent<HealthBarText>().ChangeHealthSlider(currentHealth, maxHealth);
     }
 }
