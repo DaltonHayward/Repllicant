2023cf8c76faa42c78f98f718fab4215bfedcc42
gloria-hello <105770648+gloria-hello@users.noreply.github.com>
2024-04-04 01:49:06 -0600

    Harpy Changes

--- Assets/_Scripts/Systems/StatusEffect/EffectableObject.cs
+++ Assets/_Scripts/Systems/StatusEffect/EffectableObject.cs
@@ -13,12 +13,12 @@ public class EffectableObject : MonoBehaviour
 {
     List<BaseEffect> ActiveEffects = new List<BaseEffect>();
 
-
+    
     // Update is called once per frame
     void Update()
     {
         // tick active effects - cleanup any that are finished (loops backwards on purpose)
-        for(int index = ActiveEffects.Count -1; index >= 0; --index)
+        for (int index = ActiveEffects.Count - 1; index >= 0; --index)
         {
             ActiveEffects[index].TickEffect();
 
@@ -37,14 +37,28 @@ public class EffectableObject : MonoBehaviour
 
     public void ApplyEffect(BaseEffect effectTemplate)
     {
-        // create a new instance
-        var newEffect = ScriptableObject.Instantiate(effectTemplate);
+        bool active = false;
+        for (int i = 0; i < ActiveEffects.Count; i++)
+        {
+            if (ActiveEffects[i].name == effectTemplate.name)
+            {
+                Debug.Log("EFFECT ACTIVE = TRUE");
+                active = true;
+            }
+        }
 
-        // make the effect active
-        newEffect.EnableEffect(); 
-        ActiveEffects.Add(newEffect);
+        if (active == false)
+        {
+            // create a new instance
+            var newEffect = ScriptableObject.Instantiate(effectTemplate);
+
+            // make the effect active
+            newEffect.EnableEffect();
+            ActiveEffects.Add(newEffect);
+        }
+
     }
-    
+
     // example effect
     public float Effect_MovementSpeed(float originalMovementSpeed)
     {
